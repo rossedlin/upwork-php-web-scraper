188ee16342cedaa12b47d89341c7107521bdefce
Ross Edlin <rossedlin@gmail.com>
2017-08-30 15:06:36 +0700

    after submitting to upwork

---
+++ web.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/src/webscraper.php';
+/**
+ * Created by PhpStorm.
+ * User: Ross Edlin
+ * Date: 30/08/2017
+ * Time: 14:13
+ */
+
+\CuttingWeb\pre(\CuttingWeb\placeholder([
+	'--startDate=2016-11-13',
+	'--endDate=2016-11-16',
+	'--maxResultsPerAuthor=1',
+]));
